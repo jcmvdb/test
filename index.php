52e7647a0a843f81b540696435f06ecf831d0a3c
jcmvdb <mvdb.jeroen@gmail.com>
2023-08-19 23:23:13 +0200

    added the index.php file

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+</head>
+<body>
+<?php
+
+$amount = 10;
+
+for ($i = 0; $i < $amount; $i++) {
+    echo "<hr size='10' width='10''>";
+}
+?>
+</body>
+</html>
